6e18820ee001b6094be671a6e6a5de37c7d8a713
Bikmul <Bikmullin24@mail.ru>
2023-07-02 18:34:23 +0300

    -

---
+++ day01/ex04/Program.java
@@ -0,0 +1,33 @@
+package Java.Piscine.day01.ex04;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+
+    TransactionsService service = new TransactionsService();
+    
+    service.addUser("Kolya", 2500);
+    service.addUser("Vasya", 10000);
+    service.addUser("Amir", 50000);
+    service.addUser("Gleb", 50000);
+    service.addUser("Adolf ", 100000);
+    service.addUser("Iosif", 100000);
+
+    service.printallUsers();
+    
+    service.makeTransaction(3, 1, 1000, "DEBIT");
+    service.makeTransaction(2, 1, 1000, "DEBIT"); 
+    service.makeTransaction(1, 2, 1000,"DEBIT");
+    service.makeTransaction(3, 0, 10000,"DEBIT");
+    service.makeTransaction(2, 3, 20000,"DEBIT");
+
+    service.makeTransaction(4, 2, 10000,"KREDIT");
+    service.makeTransaction(5, 4, 50000,"KREDIT");
+
+    service.getUserTransactions(4);
+    service.getUserTransactions(2);
+    service.getUserTransactions(5);
+    // service.printallTransactions();
+    service.printallUsers();
+
+    }
+}
---
+++ day01/ex04/TransactionStruct.java
@@ -0,0 +1,26 @@
+package Java.Piscine.day01.ex04;
+
+public class TransactionStruct{
+    private Transaction data;
+    TransactionStruct next;
+    TransactionStruct previous;
+
+    TransactionStruct(Transaction data){
+        this.data = data;
+    }
+    public Transaction getdata(){
+        return data;
+    }
+    public TransactionStruct getnext(){
+        return next;
+    }
+    public void setnext(TransactionStruct next){
+        this.next = next;
+    }
+    public TransactionStruct getprevious(){{
+        return previous;
+    }}
+    public void setprevious(TransactionStruct previous){
+        this.previous = previous;
+    }
+}
